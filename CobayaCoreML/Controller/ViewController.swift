c396c8af6a822c1e1dc4d925443ac3f493745f57
Sia Feri Santos <siaferi.santos@yahoo.com>
2019-01-31 14:59:32 +0700

    nambah coment

--- CobayaCoreML/Controller/ViewController.swift
+++ CobayaCoreML/Controller/ViewController.swift
@@ -114,7 +114,7 @@ class ViewController: UIViewController {
         namaBuah.text = "\(namaNamaBuah[2])"
     }
     
-    /// Setup the Cancel button, dummy image, and white circle in the middle
+    /// Setup the Cancel button, dummy image, and white circle in the middle s
     func setupDummyImage(){
         let x = view.frame.width / 2
         let y = view.frame.height
