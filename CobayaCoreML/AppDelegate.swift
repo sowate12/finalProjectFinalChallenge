b9cac519f2975396b0d41349a6253f16af3b9a53
keenan warouw <>
2019-03-04 14:08:21 +0700

    gif

--- CobayaCoreML/AppDelegate.swift
+++ CobayaCoreML/AppDelegate.swift
@@ -11,6 +11,7 @@ import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
     var window: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
